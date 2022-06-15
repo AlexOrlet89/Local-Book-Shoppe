314e7e92b7f5f5338230ff4bfac2077ac90a8a01
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-15 12:09:57 -0700

    started test for insert the right way, consolelog purge

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
+const Book = require('../lib/models/Book');
 
 describe('books routes', () => {
   beforeEach(() => {
@@ -17,6 +18,12 @@ describe('books routes', () => {
     const results = await request(app).get('/books/1');
     expect(results.body.title).toEqual('The Monster at the end of this book');
     expect(results.body.author[0].name).toEqual('Eric Hill');
+  });
+
+  it('should insert a new book into our table', async () => {
+    const book = new Book({ title: 'my book', released: 1999 });
+    const results = await request(app).post('/books/:id').send(book);
+    expect(results.status).toEqual(200);
   });
 
   afterAll(() => {
